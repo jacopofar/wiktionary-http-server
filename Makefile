ed96d3973fa347b61cdc6ec13c086376b6529311
Jacopo Farina <jacopo1.farina@gmail.com>
2022-09-23 15:52:35 +0200

    Show access, better error handling

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 run-server:
 	python3 -m venv .venv
 	.venv/bin/python3 -m pip install -r requirements.txt
-	.venv/bin/sanic wiktserver.app
+	.venv/bin/sanic --access-logs wiktserver.app
 .PHONY: run-server
--- wiktserver/__init__.py
+++ wiktserver/__init__.py
@@ -4,21 +4,23 @@ import sqlite3
 from ujson import loads
 from sanic import Sanic
 from sanic.response import json
-
+from sanic import exceptions
 
 app = Sanic("wiktionary_app")
-app.ctx.connection = sqlite3.connect("dict.db")
+app.ctx.connection = sqlite3.connect("dict_mini.db")
 app.ctx.cur = app.ctx.connection.cursor()
 
 
 @app.get("/w/<word:str>")
 async def test(request, word: str):
-    entries = loads(
-        app.ctx.cur.execute(
+    entries = app.ctx.cur.execute(
             "SELECT entries from words where word = ?", (word,)
-        ).fetchone()[0]
-    )
-    return json(entries)
+        ).fetchone()
+
+    if entries is None:
+        raise exceptions.NotFound(f'Cannot find {word}')
+    else:
+        return json(loads(entries[0]))
 
 
 if __name__ == "__main__":
