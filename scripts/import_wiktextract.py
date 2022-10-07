bba966a5c8566c1c28fe2beae64fde0d23674e48
Jacopo Farina <jacopo1.farina@gmail.com>
2022-10-07 11:58:30 +0200

    Load in multiple passages to use less RAM

--- scripts/import_wiktextract.py
+++ scripts/import_wiktextract.py
@@ -2,6 +2,8 @@ import json
 import sqlite3
 from sys import argv
 
+PARTITIONS = 6
+
 if __name__ == "__main__":
     connection = sqlite3.connect(argv[2])
     connection.execute(
@@ -10,21 +12,29 @@ if __name__ == "__main__":
             entries TEXT
         )"""
     )
-    with open(argv[1]) as fr:
-        rows = {}
-        for idx, line in enumerate(fr):
-            if idx % 100_000 == 0:
-                print(f'Read {idx} lines')
-            entry = json.loads(line)
-            word = entry.pop("word")
-            if word in rows:
-                rows[word].append(entry)
-            else:
-                rows[word] = [entry]
-    print('All definitions loaded, now writing to the DB')
-    with connection:
-        connection.executemany(
-            """INSERT INTO words(word, entries)
-                VALUES(?,?)""",
-            ((word, json.dumps(entries)) for word, entries in rows.items()),
+    for p in range(PARTITIONS):
+        print(f"Processing partition {p}")
+        with open(argv[1]) as fr:
+            rows: dict[str, list] = {}
+            for idx, line in enumerate(fr):
+                if idx % 400_000 == 0:
+                    print(
+                        f"Read {idx} lines, {len(rows)} pending rows [partition {p}/{PARTITIONS}]"
+                    )
+                entry = json.loads(line)
+                word = entry.pop("word")
+                if hash(word) % PARTITIONS != p:
+                    continue
+                if word in rows:
+                    rows[word].append(entry)
+                else:
+                    rows[word] = [entry]
+        print(
+            f"All definitions for partition {p}/{PARTITIONS} loaded, now writing to the DB"
         )
+        with connection:
+            connection.executemany(
+                """INSERT INTO words(word, entries)
+                    VALUES(?,?)""",
+                ((word, json.dumps(entries)) for word, entries in rows.items()),
+            )
